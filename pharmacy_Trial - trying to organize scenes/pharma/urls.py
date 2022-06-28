b21ad58ff1e46c0c1cbc17c6333069a36bab3359
Ibrahim Matar <54605141+hima700@users.noreply.github.com>
2022-06-28 03:15:20 +0200

    Adding Scenes
    The Registration page "working on sqlite3 not postgres still needs to be added"
    The Login page "working on sqlite3 not postgres still needs to be added"
    The view medicine page 
    The add to cart page "not implemented yet"

---
+++ pharmacy_Trial - trying to organize scenes/pharma/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'pharma'
+urlpatterns = [
+    path("index", views.IndexView.as_view(), name='index'),
+    path('new', views.CreateView.as_view(), name='create'),
+    path('cart', views.CreateView.as_view(), name='cart'),   #should implement cartview
+    path('post/<int:pk>/', views.ViewPostView, name='view'),
+    path('post/<int:pk>/update', views.UpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),
+    path('', views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+
+]
---
+++ pharmacy_Trial - trying to organize scenes/scripts/install_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script installs the dependencies of the application
+
+pip install -r requirements.txt
+
